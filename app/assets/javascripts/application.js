9650306c68c18f3d2cbd9577dffcfbfaab15fbf5
Andre Krauss <andre.kmbv@gmail.com>
2019-03-23 23:30:23 +0000

    Added template

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require jquery3
+//= require jquery_ujs
+//= require admin/js/contact
+//= require admin/js/jquery.ajaxchimp.min
+//= require admin/js/jquery.form
+//= require admin/js/jquery.validate.min
+//= require admin/js/mail-script
+//= require admin/js/main
+//= require admin/plugins/bootstrap/bootstrap.bundle.min
+//= require admin/plugins/fontawesome/bootstrap.bundle.min
+//= require admin/plugins/jquery/jquery-3.2.1.min
+//= require admin/plugins/Magnific-Popup/jquery.magnific-popup.min
+//= require admin/plugins/nice-select/jquery.nice-select.min
+//= require admin/plugins/nouislider/nouislider.min
+//= require admin/plugins/owl-carousel/owl.carousel.min
+//= require admin/plugins/themify-icons/ie7/ie7
+//= require admin/plugins/skrollr.min
+//= require admin/plugins/contact
+//= require admin/plugins/jquery.ajaxchimp.min
+//= require admin/plugins/jquery.form
+//= require admin/plugins/jquery.validate.min
+//= require admin/plugins/mail-script
+
